8b86feb14c07b8585619260a6491b933e5f0e436
Tuan Nguyen <anhtuan5297@gmail.com>
2021-06-29 12:18:33 +0700

    changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   webpack: config => {
-    config.resolve.modules.push(path.resolve('./b-play'))
+    config.resolve.modules.push(path.resolve('.'))
 
     return config
   }
@@ -10,7 +10,7 @@ module.exports = {
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: { base: '/b-play' },
+  router: { base: '/b-play/' },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Bplay',
